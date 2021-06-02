ba8533117c57df846e58ca566353f2044cd3b017
Haadi Majeed <haadi.majeed99@gmail.com>
2021-06-02 15:35:10 -0500

    commented aliases and made the the ignore include .un~ files because heck them

--- psAliases.ps1
+++ psAliases.ps1
@@ -1,8 +1,16 @@
+<#
+    .Description
+    Runs git pull for every repo in the dir
+#>
 function Git-Update {
     Push-Location;gci . -Directory|%{Set-Location $_.FullName;Write-Host $_.Name; git pull;Write-Host};Pop-Location
 }
 new-alias gitpull Git-Update
 
+<#
+    .Description
+    Runs git status for every repo in the dir
+#>
 function Git-Status {
     Push-Location;gci . -Directory|%{Set-Location $_.FullName;Write-Host $_.Name; git status;Write-Host};Pop-Location
 }
